f3d3245c846869009f1334079f4a8e9216f09837
Kapten Oleng <46507140+cyberJMB@users.noreply.github.com>
2021-12-23 16:18:15 +0700

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,4 @@
+<?php 
+$url ="https://uptocoin.my.id/fp/faucet.php?address=DK2GJfDWjrhpmTowgegM49Admxku4bdAmB&currency=DOGE&key=535a4560730b690a35459526c30a9ee5";
+$rand="Mozilla/9.0 (Mobile; Nokia 9000 4G; rv:98.0) Gecko/98.0 Firefox/98.0 KAIOS/2.5.4";
+$token = "token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYbJ3mwFhsngygAGBAsAAIMNU0KRKJLgE7R8XOjMrF3fCyQV0sqdrXJxdrY7xi16WwQBIMEYCIQD6NF7ZQ_UZo2aXfY-Rg9B6PD46XBcvPXhSpdhwD1A0MQIhAL3WPuF13kWBgJ_mipwMuyc5OxhUNfdObwgtNJ0oeO5H";
