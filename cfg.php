b071587e3523b95da29a775953ef4aff2cc44a27
Kapten Oleng <46507140+cyberJMB@users.noreply.github.com>
2021-12-28 22:21:06 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,9 +1,7 @@
 <?php 
 //url
 $url ="https://uptocoin.my.id/fp/faucet.php?address=DK2GJfDWjrhpmTowgegM49Admxku4bdAmB&currency=DOGE&key=535a4560730b690a35459526c30a9ee5";
-$url ="https://uptocoin.my.id/fp/faucet.php?address=DPvMBrDK1e8rfxJ93SwMzGv7vDcapvrnm9&currency=DOGE&key=d1e383322d85b3a0cf9d888316284e0e";
 
 //token
 $token = "token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYbKmqgFhsqcEgAGBAsAAIEH63XjIPJlQ4SbUYpuGkdNR1s1qCvxDCj6yw3uaBlyOwQBGMEQCIBhSNZdm7oyGaREYjkGtkKDBkCbB76aw7LlMixOsH0TFAiA8d4uhwNn4LH8gQ5XWVYnbt3eWVmWS2JlzLmk-Flb-NA";
-$token ="token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYcdE_wFhx0T_gAGBAsAAIKh2FK5-H_0tKs4V61GqQEAh3NvprSQvR-Maq7thN_4vwQBHMEUCIQCJcy78gaydJ6ErD3AqVLzJ7LXM0ii89PmBPTcMzL4RPQIgdT3gdPXTcDJRKYp4oMCqrI6-5K6GUrB1Xee-DwQYCTI";
 ?>
