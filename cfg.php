77a6a361024ccf9160080c8fea28264b03d5c11d
Kapten Oleng <46507140+cyberJMB@users.noreply.github.com>
2022-01-03 16:48:24 +0700

    Delete cfg.php

--- cfg.php
+++
@@ -1,7 +0,0 @@
-<?php 
-//url
-$url ="https://uptocoin.my.id/fp/faucet.php?address=DK2GJfDWjrhpmTowgegM49Admxku4bdAmB&currency=DOGE&key=535a4560730b690a35459526c30a9ee5";
-
-//token
-$token = "token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYbKmqgFhsqcEgAGBAsAAIEH63XjIPJlQ4SbUYpuGkdNR1s1qCvxDCj6yw3uaBlyOwQBGMEQCIBhSNZdm7oyGaREYjkGtkKDBkCbB76aw7LlMixOsH0TFAiA8d4uhwNn4LH8gQ5XWVYnbt3eWVmWS2JlzLmk-Flb-NA";
-?>
